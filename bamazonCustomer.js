a2496af0c34c15d0c451c5e0a05b86731c33f2dd
Dara9234 <dahmed@alumni.depaul.edu>
2019-01-09 21:48:07 -0600

    Table for the items done

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,5 @@
 var mysql = require("mysql");
-var inquirer = require("inquirer");
+//var inquirer = require("inquirer");
 
 var connection = mysql.createConnection({
   host: "localhost",
@@ -19,5 +19,82 @@ connection.connect(function(err) {
   if (err) throw err;
   console.log("Connection Successful!");
   console.log("connected as id " + connection.threadId);
- 
+ // run the start function after the connection is made to prompt the user
+ displayTable();
 });
+
+// Creating a function displayTable to Show all the items on sale
+
+var displayTable= function afterConnection() {
+
+
+          connection.query("SELECT * FROM products", function(err, res) {
+            if (err) throw err;
+          
+            //results of the sql select statement
+            for(var i=0; i<res.length; i++){
+                (res[i].item_id+""+res[i].product_name+""+
+             res[i].department_name+""+res[i].price+" "+res[i].stock_quantity+"\n");
+             console.table(res);
+            }  
+        });
+
+    }
+        
+
+         
+           //prompting the buyer to enter the item they want to buy
+           //inquirer
+           //.prompt({
+             //name: "choice",
+             //type: "rawlist",
+             //message: "What item would you like to buy? Please enter the item id",
+             
+           //})
+           //.then(function(answer) {
+             // based on their answer, either call the bid or the post functions
+             //for (var i=0;i<res.length;i++){
+                //if (answer.choice=== res[i].item_id) {
+                    //var product=answer.choice;
+
+             //}
+             
+             
+
+            
+     
+    
+        
+        
+         
+   
+
+// creating two prompts for customer questions
+// The first should ask them the ID of the product they would like to buy.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- bamazonSeeds.sql
+++ bamazonSeeds.sql
@@ -6,7 +6,7 @@ USE bamazon;
 
 CREATE TABLE products
 (
-    item_id INT,
+    item_id INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(45) NULL,
     department_name VARCHAR(45) NULL,
     price DECIMAL(10,2) NULL,
@@ -14,46 +14,47 @@ CREATE TABLE products
     PRIMARY KEY (item_id)
 );
 
+
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
+    (product_name,department_name, price, stock_quantity)
 VALUES
-    (3391, "Paper Towel Holder", "Home", 24.50, 100);
+    ("Paper Towel Holder", "Home", 24.50, 100);
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(3392, "USB to Micro-USB Cable", "Devices", 24.50, 50);
+    (product_name,department_name, price, stock_quantity)
+VALUES("USB to Micro-USB Cable", "Devices", 24.50, 50);
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(3393, "HP Printer Paper", "Office Products", 17.99, 100);
+    (product_name,department_name, price, stock_quantity)
+VALUES("HP Printer Paper", "Office Products", 17.99, 100);
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(3395, "Microfiber Dish Drying Mat", "Home", 5.99, 150);
+    (product_name,department_name, price, stock_quantity)
+VALUES("Microfiber Dish Drying Mat", "Home", 5.99, 150);
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(3397, "Vitamin C Serum ", "Beauty", 15.92, 200);
+    (product_name,department_name, price, stock_quantity)
+VALUES("Vitamin C Serum ", "Beauty", 15.92, 200);
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(2296, "Digtal Bathroom Scale", "Health", 20.96, 49);
+    (product_name,department_name, price, stock_quantity)
+VALUES("Digtal Bathroom Scale", "Health", 20.96, 49);
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(2298, "Smoke Detector", "Home Improvement", 74.51, 10);
-
-
-INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(2290, "Wireless Headphones", "Electronics", 119.00, 100);
+    (product_name,department_name, price, stock_quantity)
+VALUES("Smoke Detector", "Home Improvement", 74.51, 10);
 
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(3320, "Screen Protector", "Cell Phones & Accessories", 10.99, 300);
+    (product_name,department_name, price, stock_quantity)
+VALUES("Wireless Headphones", "Electronics", 119.00, 100);
 
 
 INSERT INTO products
-    (item_id,product_name,department_name, price, stock_quantity)
-VALUES(3321, "Beanie Headband", "Clothing,Shoes & Jewelry", 2.63, 30);
+    (product_name,department_name, price, stock_quantity)
+VALUES("Screen Protector", "Cell Phones & Accessories", 10.99, 300);
+
+
+INSERT INTO products
+    (product_name,department_name, price, stock_quantity)
+VALUES("Beanie Headband", "Clothing,Shoes & Jewelry", 2.63, 30);
